29b9fb52ea2b6533a564a6a9ca06212fde84be4a
thermarthae <thermarthae@protonmail.com>
2018-10-11 18:56:16 +0200

    add PubSub TODO/warning

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -5,6 +5,17 @@ import queries from './queries';
 import mutations from './mutations';
 import subscriptions from './subscriptions';
 
+/**
+ * Note that the default PubSub implementation is intended for demo purposes.
+ * It only works if you have a single instance of your server and doesn't scale beyond a couple of connections.
+ * For production usage you'll want to use one of the PubSub implementations backed by an external store.
+ *
+ * If possble memory leak warning: (pubsub as any).ee.setMaxListeners(30);
+ *
+ * //TODO: Replace with graphql-redis-subscriptions or graphql-mqtt-subscriptions
+ * https://github.com/davidyaha/graphql-mqtt-subscriptions
+ * https://github.com/davidyaha/graphql-redis-subscriptions
+ */
 export const pubsub = new PubSub();
 
 export default new GraphQLSchema({
