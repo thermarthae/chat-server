9c48da9e28e9baf18b2edb69eb893f1f2b52bf3b
thermarthae <thermarthae@protonmail.com>
2018-10-26 19:03:05 +0200

    fix mutation GraphQLFieldConfigMap type error

--- src/graphql/mutations/index.ts
+++ src/graphql/mutations/index.ts
@@ -1,9 +1,7 @@
 import userMutation = require('./user.mutations');
 import conversationMutation = require('./conversation.mutations');
 
-import { GraphQLFieldConfigMap } from 'graphql';
-
 export default {
 	...userMutation,
 	...conversationMutation
-} as GraphQLFieldConfigMap<any, any>;
+};
