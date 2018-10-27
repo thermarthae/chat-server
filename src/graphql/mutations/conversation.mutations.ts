b1a53becea91c867bfa11cee7d76cbea89cce907
thermarthae <thermarthae@protonmail.com>
2018-10-27 17:02:38 +0200

    add markConversationAsRead mutation

--- src/graphql/mutations/conversation.mutations.ts
+++ src/graphql/mutations/conversation.mutations.ts
@@ -138,3 +138,24 @@ export const sendMessage: GraphQLFieldConfig<IRootValue, IContext, ISendMessageA
 		return parsedMessage;
 	}
 };
+
+export const markConversationAsRead: GraphQLFieldConfig<IRootValue, IContext, { conversationId: string }> = {
+	type: GraphQLString,
+	description: 'Mark given conversation as read',
+	args: {
+		conversationId: {
+			type: new GraphQLNonNull(GraphQLID),
+			description: 'Conversation Id'
+		},
+	},
+	resolve: async ({ }, { conversationId }, { sessionOwner, convIDLoader }) => {
+		const verifiedUser = checkIfNoSessionOwnerErr(sessionOwner);
+		await checkUserRightsToConv(conversationId, verifiedUser, convIDLoader);
+		const status = await ConversationModel.updateOne(
+			{ _id: conversationId },
+			{ $set: { 'seen.$[s].time': new Date() } },
+			{ arrayFilters: [{ 's.user': verifiedUser._id }] }
+		);
+		return status.ok === 1 ? 'Success!' : null;
+	}
+};
