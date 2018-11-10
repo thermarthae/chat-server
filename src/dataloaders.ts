44bd069ab6d0b77af78022f8584f280463a4eef5
thermarthae <thermarthae@protonmail.com>
2018-11-10 20:02:38 +0100

    add new dataloaders errors

--- src/dataloaders.ts
+++ src/dataloaders.ts
@@ -1,8 +1,8 @@
 import mongoose = require('mongoose');
 import DataLoader = require('dataloader');
-import { ApolloError, toApolloError } from 'apollo-server-core';
+import { ApolloError, toApolloError, ForbiddenError } from 'apollo-server-core';
 
-import UserModel, { IUser } from './models/user';
+import UserModel, { IUser, UserErrors } from './models/user';
 import ConversationModel, { IConversation } from './models/conversation';
 
 export type TUserLoader = DataLoader<string, IUser>;
@@ -14,14 +14,19 @@ export interface IDataLoaders {
 }
 
 export const userIDFn = async (ids: Array<{}>) => {
+	const error = new ApolloError(
+		UserErrors.UserNotExistsError,
+		'UserNotExistsError',
+	);
+
 	const result = await UserModel.find({ _id: { $in: ids } })
 		.cache(30)
 		.catch(err => {
-			if (err.name === 'CastError') throw new ApolloError('404 (Not Found)', 'NotFound');
+			if (err.name === 'CastError') throw error;
 			throw toApolloError(err);
 		}) as IUser[];
 
-	if (!result[0]) throw new ApolloError('404 (Not Found)', 'NotFound');
+	if (!result[0]) throw error;
 	return result;
 };
 
@@ -31,7 +36,7 @@ export const convIDFn = async (ids: Array<{}>) => {
 		{ $lookup: { from: 'User', localField: 'users', foreignField: '_id', as: 'users' } },
 	]).cache(30);
 
-	if (!result[0]) throw new ApolloError('404 (Not Found)', 'NotFound');
+	if (!result[0]) new ForbiddenError('Conversation does not exist or access denied');
 	return result;
 };
 
